29ab3a58ec61a468a49c968be3d9eabb03516650
chemadent <b00902002@ntu.edu.tw>
2017-09-27 03:13:21 +0800

    drafts, list of the unpublished, publish button, delete post, login, logout, security, comment

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+		'ENGINE': 'django.db.backends.postgresql',
+		'NAME': 'myfirstblog',
+		'USER': 'chemadent',
+		'PASSWORD': 'password of chemadent',
+		'HOST': 'localhost',
+		'PORT': '',
+	}
 }
 
 
@@ -120,3 +124,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGIN_REDIRECT_URL = '/'
